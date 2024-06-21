3f4979a536a67a0b102f3bb75925120dac8be1c6
devprinceng <devprinceng@gmail.com>
2024-06-21 14:45:12 +0100

    finished routing task

--- index.js
+++ index.js
@@ -3,8 +3,30 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+//parse request to json
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.post('/example', (req, res) => {
+    let data = req.body;
+    console.log(data);
+});
+
+app.get('/example', (req, res) => {
+    res.send('This is GET method')
+});
+
+app.put('/example', (req, res) => {
+    
+    res.send('this is put method');
+});
+
+app.delete('/example', (req, res) => {
+    
+    res.send('this is delete method');
 });
 
 app.listen(3000, console.log(`Server running at Port ${PORT}`));
