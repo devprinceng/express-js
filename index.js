aad3ad1dff0c30d95d450932b5c3cfba97b1c245
devprinceng <devprinceng@gmail.com>
2024-07-07 01:31:36 +0100

    reviewed ejs, middlewares, routing, cookies

--- index.js
+++ index.js
@@ -1,38 +1,52 @@
 const express = require("express");
-
 const app = express();
 const PORT = 3000;
+const cookieParser = require('cookie-parser');
+const { render } = require("ejs");
 // create student subroute
 let students = express.Router();
 
 //parse request to json
 app.use(express.json())
-
-//use student route
-app.use('/students', students)
+app.use(cookieParser());
+// set view enginge
+app.set("view engine", "ejs");
+//middleware
+const middleware1 = (req, res, next) => {
+    console.log('Middleware One');
+    next()
+}
+// use middleware
+app.use(middleware1)  ;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+    res.render('pages/index.ejs')
+})
 
-app.get('/example/:userId', (req, res) => {
-    let { userId } = req.params;
-    let { name, role } = req.query;
-    
-    console.log("name:", name);
-    console.log("role:", role);
+app.get('/test', (req, res) => {
+    res.render('test.ejs', {
+        name: 'Dev Prince NG'
+    })
+})
 
-    res.send(`User ID:  ${ userId }`);
-});
-
-// test students subroute and retrieve path, base url, details etc
-students.get('/example/', (req, res) => {
-    console.log('base url:', req.baseUrl);
-    console.log('original url:', req.originalUrl);
-    console.log('path:', req.path);
-
-    res.send('This is student get route');
-});
-
+app.get('/example',(req, res) =>{
+    res.format({
+        'text/plain': ()=>{
+            res.send('hello world')
+        },
+        'text/html' : ()=> {
+            res.render('pages/index.ejs')
+        },
+        'application/json': ()=> {
+            res.send({
+                name: "Dev Prince NG",
+                email: "devprinceng@gmail.com"
+            })
+        },
+        default: () => {
+            res.send('Nothing Matched')
+        }
+    })
+})
 
 app.listen(3000, console.log(`Server running at Port ${PORT}`));
