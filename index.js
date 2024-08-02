e073c9d60191e9c73d66d6fdd4bb7d2adf7daace
devprinceng <devprinceng@gmail.com>
2024-08-02 06:38:18 +0100

    completed Student schema and model setup

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ const PORT = 3000;
 //* connection url
 const connectionUrl = 'mongodb://localhost:27017/SchoolDb';
 mongoose.connect(connectionUrl).then( () => console.log('Database connection successful')).catch((error) => res.status(500).send(error.message));
+
+//setup student Schema
+StudentSchema = mongoose.Schema({
+    name: String,
+    regno: String,
+    department: String,
+    age: Number,
+    phone: String,
+    email: String,
+});
+//setup student model
+Student = mongoose.model('student', StudentSchema);
+
 const errorMiddleware = (error, req, res, next) => {
     res.status(500).send(error.message);
 };
