9ee9696e434031f8b638a7749a8a41cfb3ba8363
devprinceng <devprinceng@gmail.com>
2024-08-03 04:23:05 +0100

    added update methods: findById, findOneAndUpdate

--- index.js
+++ index.js
@@ -21,6 +21,16 @@ StudentSchema = mongoose.Schema({
 //setup student model
 Student = mongoose.model('student', StudentSchema);
 
+// read students
+app.get('/students', async(req, res, next) => {
+    try {
+        const students = await Student.find();
+        res.status(200).send(students);
+    } catch (error) {
+        res.status(500).send(error)
+    }
+    
+})
 // add student to database
 app.post('/students/single', async (req, res, next) => {
     try {
@@ -54,6 +64,36 @@ app.post('/students/multiple', async (req, res, next) => {
         res.status(500).send(error);
     }
 })
+// find one and update
+app.put('/students', async(req, res, next) => {
+    try {
+        const { email } = req.query;
+        const { department } = req.body;
+        //fnd and update
+        const student = await Student.findOneAndUpdate({email}, {department}).then(() => res.status(200).send('Student Updated Successfully'));
+    } catch (error) {
+      res.status(500).send(error);
+    }
+})
+
+// find by id
+app.put('/students/:email', async(req, res, next) => {
+    try {
+        const { email } = req.params;
+        const { department } = req.body;
+
+        // const student= await Student.findById(id);
+        const student= await Student.findOne({email});
+        student.department = department;
+        await student.save();
+
+        res.status(200).send('Student Updated Successfully');
+    } catch (error) {
+      res.status(500).send(error);
+    }
+})
+
+//error middleware
 const errorMiddleware = (error, req, res, next) => {
     res.status(500).send(error.message);
 };
